9591ab2d0594dbdcc6f5804d3222ebcee0f51d76
marlockd <denislogvinov42@gmail.com>
2019-06-03 12:18:50 +0300

    Initial commit

---
+++ src/main/kotlin/ru/spbstu/kotlintask/ls_main.kt
@@ -0,0 +1,144 @@
+package ru.spbstu.kotlintask
+
+import com.github.ajalt.clikt.core.NoRunCliktCommand
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import org.apache.commons.io.comparator.NameFileComparator
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.attribute.BasicFileAttributes
+import java.util.*
+
+/**
+ * Вариант 1 -- ls
+Вывод содержимого указанной в качестве аргумента директории в виде
+отсортированного списка имен файлов.
+● Флаг -l (long) переключает вывод в длинный формат, в котором, кроме имени
+файла, указываются права на выполнение/чтение/запись в виде битовой маски
+XXX, время последней модификации и размер в байтах.
+● Флаг -h (human-readable) переключает вывод в человеко-читаемый формат
+(размер в кило-, мега- или гигабайтах, права на выполнение в виде rwx).
+● Флаг -r (reverse) меняет порядок вывода на противоположный.
+● Флаг -o (output) указывает имя файла, в который следует вывести результат;
+если этот флаг отсутствует, результат выводится в консоль.
+В случае, если в качестве аргумента указан файл, а не директория, следует вывести
+информацию об этом файле.
+Command Line: ls [-l] [-h] [-r] [-o output.file] directory_or_file
+Кроме самой программы, следует написать автоматические тесты к ней
+ */
+fun main(inputStr : String) : Any {
+    val input = inputStr.split(" ").toMutableList()
+    val inputDir = input[input.size - 1]
+    input.removeAt(input.size - 1)
+    val inputInfo = input.toTypedArray()
+    val args = inputInfo.parse()
+    val toOut : MutableList<String>
+    if (!args.initFlag) throw IllegalArgumentException("Отсутствует флаг инициализации -ls")
+
+    fun short(path : Path): MutableList<String> {
+        val listOfFiles = mutableListOf<String>()
+        val files = File("$path").listFiles()
+        if (args.rFlag) Arrays.sort(files, NameFileComparator.NAME_REVERSE)
+        else Arrays.sort(files, NameFileComparator.NAME_COMPARATOR)
+        files.forEach {
+            if (it.isFile){
+                if(!args.hFlag) listOfFiles.add("${it.name} ${it.length()}")
+                else {
+                    var length :Float = it.length().toFloat()
+                    if (length > 1023) { length /= 1024
+                        if (length > 1023) {length /= 1024
+                            if (length > 1023) {length/= 1024
+                                listOfFiles.add("${it.name} $length Гбайт")
+                            }
+                            else listOfFiles.add("${it.name} $length Мбайт")
+                        }
+                        else listOfFiles.add("${it.name} $length Кбайт")
+                    }
+                    else listOfFiles.add("${it.name} $length байт")
+                }
+            }
+            else listOfFiles.add("dir ${it.name}")
+        }
+        return listOfFiles
+    }
+
+    fun long(path: Path): MutableList<String> {
+        val files = File("$path").listFiles()
+        var attr: BasicFileAttributes
+        val listOfFiles = mutableListOf<String>()
+        if (args.rFlag) Arrays.sort(files, NameFileComparator.NAME_REVERSE)
+        else Arrays.sort(files, NameFileComparator.NAME_COMPARATOR)
+        files.forEach {
+            if (it.isFile) {
+                attr = Files.readAttributes<BasicFileAttributes>(it.toPath(), BasicFileAttributes::class.java)
+                var read = 0
+                var readc = '-'
+                var write = 0
+                var writec = '-'
+                var execute = 0
+                var executec = '-'
+                if (it.canRead()) {read = 1
+                readc = 'r'}
+                if (it.canWrite()) {write = 1
+                writec = 'w'}
+                if (it.canExecute()) {execute = 1
+                executec = 'x'}
+                if (!args.hFlag)
+                    listOfFiles.add("${it.name} $execute$read$write ${attr.lastModifiedTime()} ${attr.size()}")
+                else {
+                    var length :Float = attr.size().toFloat()
+                    var modTime = attr.lastModifiedTime().toString().split("T")[1].split(".")[0]
+                    var modDate = attr.lastModifiedTime().toString().split("T")[0]
+                    if (length > 1023) { length /= 1024
+                        if (length > 1023) {length /= 1024
+                            if (length > 1023) {length/= 1024
+                                listOfFiles.add("${it.name} $readc$writec$executec $modDate $modTime $length Гбайт")
+                            }
+                            else listOfFiles.add("${it.name} $readc$writec$executec $modDate $modTime $length Мбайт")
+                        }
+                        else listOfFiles.add("${it.name} $readc$writec$executec $modDate $modTime $length Кбайт")
+                    }
+                    else listOfFiles.add("${it.name} $readc$writec$executec $modDate $modTime $length байт")
+                }
+            } else listOfFiles.add("dir ${it.name}")
+        }
+        return listOfFiles
+    }
+    toOut = if (args.lFlag) long(Paths.get(inputDir))
+    else short(Paths.get(inputDir))
+    return if (args.outputDir != null) outputToFile(toOut, args.outputDir.toString())
+    else toOut
+
+}
+
+class Args : NoRunCliktCommand() {
+    val initFlag : Boolean by option("-ls").flag("-LS",default = false)
+    val lFlag: Boolean by option("-l").flag("-L", default = false)
+    val hFlag: Boolean by option("-h").flag("-H", default = false)
+    val rFlag:Boolean by option("-r").flag("-R", default = false)
+    val outputDir: String? by option("-o")
+}
+
+fun Array<String>.parse(): Args {
+    val args = Args()
+    args.parse(this)
+    return args
+}
+
+
+fun output(toout: MutableList<String>) {
+    toout.forEach {
+        println(it)
+    }
+}
+
+fun outputToFile(toOut: MutableList<String>,name :String) {
+    val writer = File(name).bufferedWriter()
+        toOut.forEach {
+            writer.write(it)
+            writer.newLine()
+        }
+    writer.close()
+}
---
+++ src/test/kotlin/ru/spbstu/kotlintask/Ls_mainKtTest.kt
@@ -0,0 +1,55 @@
+package ru.spbstu.kotlintask
+
+import org.junit.Assert.*
+
+class Ls_mainKtTest {
+
+    @org.junit.Test
+    fun main() {
+        assertEquals(
+            mutableListOf("1.txt rwx 2019-06-02 21:12:21 905.0 байт",
+                "10.txt rwx 2019-04-12 00:14:41 0.0 байт",
+                "12.txt rwx 2019-04-12 00:14:56 0.0 байт",
+                "2.txt rwx 2019-06-02 21:12:26 4.0 байт",
+                "20.txt rwx 2019-06-02 21:09:41 3.0 байт",
+                "21.txt rwx 2019-06-02 21:09:35 53.0 байт",
+                "3.txt rwx 2019-04-12 00:14:33 0.0 байт",
+                "4.txt rwx 2019-04-12 00:14:36 0.0 байт",
+                "videoplayback.mp4 rwx 2019-04-12 00:44:40 727.93164 Кбайт")
+            , main("-ls -l -h D:\\\\kotlinTask\\InputFiles"))
+        assertEquals(
+            mutableListOf("1.txt 905.0 байт",
+                "10.txt 0.0 байт",
+                "12.txt 0.0 байт",
+                "2.txt 4.0 байт",
+                "20.txt 3.0 байт",
+                "21.txt 53.0 байт",
+                "3.txt 0.0 байт",
+                "4.txt 0.0 байт",
+                "videoplayback.mp4 727.93164 Кбайт")
+            , main("-ls -h D:\\\\kotlinTask\\InputFiles"))
+        assertEquals(
+            mutableListOf("1.txt 111 2019-06-02T21:12:21.101309Z 905",
+                "10.txt 111 2019-04-12T00:14:41.193967Z 0",
+                "12.txt 111 2019-04-12T00:14:56.901865Z 0",
+                "2.txt 111 2019-06-02T21:12:26.465616Z 4",
+                "20.txt 111 2019-06-02T21:09:41.072156Z 3",
+                "21.txt 111 2019-06-02T21:09:35.406832Z 53",
+                "3.txt 111 2019-04-12T00:14:33.525528Z 0",
+                "4.txt 111 2019-04-12T00:14:36.769714Z 0",
+                "videoplayback.mp4 111 2019-04-12T00:44:40.681892Z 745402")
+            , main("-ls -l D:\\\\kotlinTask\\InputFiles"))
+        assertEquals(
+            mutableListOf("1.txt 905",
+                "10.txt 0",
+                "12.txt 0",
+                "2.txt 4",
+                "20.txt 3",
+                "21.txt 53",
+                "3.txt 0",
+                "4.txt 0",
+                "videoplayback.mp4 745402")
+            , main("-ls D:\\\\kotlinTask\\InputFiles"))
+
+    }
+}
